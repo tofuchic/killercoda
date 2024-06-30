d8b0ad2d143f013f01409ccb379bcfe29db65936
tofuchic <tofuchic@protonmail.com>
2024-06-30 08:18:13 +0000

    セットアップ時のスクリプトを修正

--- openllmetry-quickstart/setup.sh
+++ openllmetry-quickstart/setup.sh
@@ -1,4 +1,17 @@
 git clone https://github.com/traceloop/openllmetry.git
-curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
+cd openllmetry/packages/sample-app
 
+curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
+source "$HOME/.rye/env"
+rye sync
+python --version | echo > /tmp/background0
+
+python -m ensurepip --upgrade
+python -m pip install --upgrade pip
+pyhton -m pip --version | echo > /tmp/background0
+
+python -m pip install poetry
+pyhton -m poetry --version | echo > /tmp/background0
+
+python -m poetry install
 echo done > /tmp/background0
