5f2f729689f61d1a1adb4b530661d00a7a4af6b9
tofuchic <tofuchic@protonmail.com>
2024-07-31 17:28:00 +0900

    Fix/preinstall (#12)

--- openllmetry-quickstart/intro.md
+++ openllmetry-quickstart/intro.md
@@ -7,4 +7,10 @@ Let's get started.
 In this scenario, you will run the example applications of OpenLLMetry.
 The example applications code exists in [the repository](https://github.com/traceloop/openllmetry/tree/main/packages/sample-app), so you first have to clone it.
 
-<!-- 事前セットアップでpython周りがインストール完了すること、doneと出るまで待ってほしいことを記載 -->
+For python execution, `[rye](https://rye.astral.sh/)` is under installing.
+Please wait until the terminal echo `Hello`.
+
+```bash
+source ~/.bashrc
+cd openllmetry/packages/sample-app
+```{{exec}}
--- openllmetry-quickstart/setup.sh
+++ openllmetry-quickstart/setup.sh
@@ -2,16 +2,17 @@ git clone https://github.com/traceloop/openllmetry.git
 cd openllmetry/packages/sample-app
 
 curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
+echo 'source "$HOME/.rye/env"' >> ~/.bashrc
 source "$HOME/.rye/env"
 rye sync
-python --version | echo > /tmp/background0
+python --version
 
 python -m ensurepip --upgrade
 python -m pip install --upgrade pip
-python -m pip --version | echo > /tmp/background0
+python -m pip --version
 
 python -m pip install poetry
-python -m poetry --version | echo > /tmp/background0
+python -m poetry --version
 
 python -m poetry install
 echo done > /tmp/background0
